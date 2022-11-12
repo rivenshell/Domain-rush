42c87e2cce7d23ae7fbea6015be3ecd2c8996fb6
Rivenshell <rivenshell111@gmail.com>
2022-11-12 17:22:11 -0500

    App Update

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "my", " "];
+  let adj = [
+    "great",
+    "big",
+    "average",
+    "blue",
+    "clever",
+    "calm",
+    "real",
+    "silly",
+    "shiny",
+    "super"
+  ];
+  let noun = [
+    "wife",
+    "racoon",
+    "device",
+    "psychology",
+    "power",
+    "apple",
+    "tongue"
+  ];
+
+  function pulldomain(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  let secone = pulldomain(pronoun);
+  let sectwo = pulldomain(adj);
+  let secthree = pulldomain(noun);
+
+  let result = secone + sectwo + secthree + ".com";
+  document.getElementById("root").innerHTML = result;
+
+  // document.getElementById("excuse").innerHTML = pullexcuse(what);
+  console.log("is my code working");
 };
